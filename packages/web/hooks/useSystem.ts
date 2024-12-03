baf8fa9869bc5e5520ff54e92f96ff0a3eefe446
刘昊 <liu.hao1443@gmail.com>
2024-12-03 10:04:33 +0800

    merge v4.8.12-fix

--- packages/web/hooks/useSystem.ts
+++ packages/web/hooks/useSystem.ts
@@ -2,6 +2,7 @@ import { useMediaQuery } from '@chakra-ui/react';
 
 export const useSystem = () => {
   const [isPc] = useMediaQuery('(min-width: 900px)');
+  // const [isPc] = [false];
 
   return { isPc };
 };
